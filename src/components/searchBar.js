3610fd3886907fb48082ab4474b0c51b07de96ff
Isra-git <villar_80@hotmail.com>
2026-01-05 18:06:04 +0100

    Add ReduxForm, react-router, and HOC withRouter skeleton for navigation

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+// importamos redux
+import { Field, reduxForm } from "redux-form";
+
+// para dirigir a otra pagina
+import { withRouter } from "react-router-dom";
 class SearchBar extends Component {
-    render() {
-        return (
-            <form>
-               <input placeholder="Search DailySmarty"/>
-            </form>
-        )
-    }
+  handleFormSubmit = function ({ query }) {
+    console.log("HandleSubmit for query: ", query);
+    //Navegar a otra ruta
+    this.props.history.push("/results");
+  };
+
+  renderInput(field) {
+    return (
+      <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form
+        className="search-bar"
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+      >
+        <Field name="query" component={this.renderInput} />
+      </form>
+    );
+  }
 }
+SearchBar = reduxForm({
+  form: "searchBar",
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
