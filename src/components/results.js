eaa863bde57fd828e28d4f8af34c4ec92508c0c8
Isra-git <villar_80@hotmail.com>
2026-01-22 12:36:38 +0100

    Sidebar Search Filters skeleton

--- src/components/results.js
+++ src/components/results.js
@@ -6,6 +6,8 @@ import Logo from "./logo";
 import SearchBar from "./searchBar";
 import ResultsSearchPosts from "./resultsSearchPosts";
 
+import { MdManageSearch } from "react-icons/md";
+
 const Results = (props) => {
   // inicializamos el dispatch
   const dispatch = useDispatch();
@@ -21,8 +23,21 @@ const Results = (props) => {
       <div className="result-search-bar">
         <SearchBar handleSearchSubmit={handleFormSubmit} />
       </div>
-      <div className="result-search-posts">
-        <ResultsSearchPosts />
+      <div className="results-content-container">
+        {/* columna izquierda*/}
+        <aside className="results-sidebar">
+          <div className="sidebar-title">
+            <span>Filtros de Busqueda</span>
+            <span>
+              <MdManageSearch size={25} />
+            </span>
+          </div>
+          {/* logica de filtro de busqueda*/}
+        </aside>
+        {/* columna derecha */}
+        <div className="result-search-posts">
+          <ResultsSearchPosts />
+        </div>
       </div>
     </div>
   );
